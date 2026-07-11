59c1b1f7971f7d77639082158e2f1a0088656b73
lief-agent <github@lief.re>
2026-07-11 14:51:30 +0200

    MachO: avoid an abort();

--- src/MachO/ChainedFixup.cpp
+++ src/MachO/ChainedFixup.cpp
@@ -113,7 +113,7 @@ bool chained_fixup::is_rebase(uint16_t ptr_format) const {
     default:
     {
       LIEF_ERR("Unknown pointer format: {:#06x}", ptr_format);
-      std::abort();
+      return false;
     }
   }
 }
